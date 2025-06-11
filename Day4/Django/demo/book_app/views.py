a20a3494f6cb9884b053b25aa51bbff5f8f5afd7
Ninad-Badekar <ninad.badekar@neosoftmail.com>
2025-06-11 18:19:04 +0530

    Django

---
+++ Day4/Django/demo/book_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
